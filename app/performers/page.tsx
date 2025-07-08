5b705121cbf491787c7b1f1db171e2fa31dfcf52
Nroling <rolingnick@gmail.com>
2025-07-08 14:40:10 -0500

    Remove Performers link from navigation and update Performers page

--- app/performers/page.tsx
+++ app/performers/page.tsx
@@ -1,25 +1,14 @@
 import "../animations.css";
 import Tutorials from "../tutorials/page";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function PerformersPage() {
   return (
     <div style={{ position: "relative", overflow: "hidden", minHeight: "100vh" }}>
       {/* Navigation */}
       <nav className="relative z-20 w-full p-4 flex justify-between items-center bg-black/70">
-        <Link href="/" className="flex items-center">
-          <div className="relative h-8 w-auto">
-            <Image
-              src="/images/boostify-logo.png"
-              alt="Boostify Tunes Logo"
-              width={100}
-              height={30}
-              className="object-contain"
-              priority
-            />
-          </div>
-        </Link>
+        {/* Logo removed */}
+        <div />
         <div className="flex gap-6">
           {/* Performers link removed */}
           <Link href="/about" className="hover:text-gray-300 transition-colors">
